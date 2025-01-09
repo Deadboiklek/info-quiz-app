496a1dd0bde775572d2b04bfd7ab611d4aa4355e
Deadboiklek <91292342+Deadboiklek@users.noreply.github.com>
2025-01-09 22:48:42 +0200

    Добавлен экран Login, нужно доделать, подумать над viewmodel

---
+++ app/src/main/java/com/example/infoquizapp/view/screen/LoginScreen.kt
@@ -0,0 +1,97 @@
+package com.example.infoquizapp.view.screen
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun LoginScreen(){
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        //Email textfield
+        OutlinedTextField(
+            value = "Тут сделать",
+            onValueChange = {TODO()},
+            label = { Text("Email") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            singleLine = true
+        )
+
+        //Password textfield
+        OutlinedTextField(
+            value = "Тут сделать",
+            onValueChange = {TODO()},
+            label = { Text("Password") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            singleLine = true
+        )
+
+        //Forgot passsword
+        Text(
+            text = "Forgot password?",
+            modifier = Modifier
+                .align(Alignment.End)
+                .clickable{TODO()},
+            style = TextStyle(color = MaterialTheme.colorScheme.tertiary, fontSize = 14.sp)
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        //Log in button
+        Button(
+            onClick = {TODO()},
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp),
+            shape = RoundedCornerShape(8.dp)
+        ) {
+            Text(text = "Log in", fontSize = 16.sp, fontWeight = FontWeight.Bold)
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Sign up Text
+        Text(
+            text = "Sign up",
+            modifier = Modifier
+                .clickable { TODO() },
+            style = TextStyle(
+                color = MaterialTheme.colorScheme.secondary,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            )
+        )
+    }
+}
