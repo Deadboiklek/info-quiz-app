13874a98914634a9b9bd8be3db132d3180e2ec89
Deadboiklek <91292342+Deadboiklek@users.noreply.github.com>
2025-01-11 19:58:05 +0200

    Добавлен экран Sign up, в экран Login добавлен Text

--- app/src/main/java/com/example/infoquizapp/MainActivity.kt
+++ app/src/main/java/com/example/infoquizapp/MainActivity.kt
@@ -4,15 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.compose.AppTheme
 import com.example.infoquizapp.view.screen.LoginScreen
+import com.example.infoquizapp.view.screen.SignUpScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,21 +17,5 @@ class MainActivity : ComponentActivity() {
 
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppTheme() {
-        Greeting("Android")
     }
 }
