e0df4ddce10411b0830b2c1da253c89faba17fb9
Deadboiklek <91292342+Deadboiklek@users.noreply.github.com>
2025-01-11 23:15:54 +0200

    Добавлен компонент TabBar для MainScreen

--- app/src/main/java/com/example/infoquizapp/MainActivity.kt
+++ app/src/main/java/com/example/infoquizapp/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.compose.AppTheme
 import com.example.infoquizapp.view.component.mainscreencomponent.ActivityCard
+import com.example.infoquizapp.view.component.mainscreencomponent.TabBar
 
 class MainActivity : ComponentActivity() {
 
@@ -14,7 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppTheme() {
-                ActivityCard()
+
             }
         }
     }
