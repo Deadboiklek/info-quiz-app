212cb17c51e178466750fa1e39ae8331083bf0af
Deadboiklek <91292342+Deadboiklek@users.noreply.github.com>
2025-01-11 20:58:08 +0200

    Добавлен компонент AppBar для MainScreen

--- app/src/main/java/com/example/infoquizapp/MainActivity.kt
+++ app/src/main/java/com/example/infoquizapp/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.compose.AppTheme
+import com.example.infoquizapp.view.component.mainscreencomponent.AppBar
 import com.example.infoquizapp.view.screen.LoginScreen
 import com.example.infoquizapp.view.screen.SignUpScreen
 
---
+++ app/src/main/java/com/example/infoquizapp/view/component/mainscreencomponent/AppBarComp.kt
@@ -0,0 +1,68 @@
+package com.example.infoquizapp.view.component.mainscreencomponent
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar(userName: String) {
+    TopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                // Аватар пользователя
+                Image(
+                    painter = painterResource(id = android.R.drawable.ic_menu_camera), // Тут надо заменить
+                    contentDescription = "Profile Picture",
+                    modifier = Modifier
+                        .size(40.dp)
+                        .background(Color.Gray, CircleShape),
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                // Имя пользователя
+                Text(text = userName,
+                    style = TextStyle(fontSize = 18.sp, fontWeight = FontWeight.Bold),
+                    modifier = Modifier.clickable { TODO( "Сделать навигацию на профиль" )}
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = { /* TODO: Настройки */ }) {
+                Icon(
+                    painter = painterResource(id = android.R.drawable.ic_menu_manage), // Тут надо заменить
+                    contentDescription = "Settings"
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    )
+}
