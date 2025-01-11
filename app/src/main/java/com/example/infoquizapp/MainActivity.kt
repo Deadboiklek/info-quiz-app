5b680d1e6ec9d38e08bb5e4781950c4b53ee011f
Deadboiklek <91292342+Deadboiklek@users.noreply.github.com>
2025-01-11 21:56:43 +0200

    Добавлен компонент ProgressBar для MainScreen

--- app/src/main/java/com/example/infoquizapp/MainActivity.kt
+++ app/src/main/java/com/example/infoquizapp/MainActivity.kt
@@ -5,11 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.compose.AppTheme
-import com.example.infoquizapp.view.component.mainscreencomponent.AppBar
-import com.example.infoquizapp.view.screen.LoginScreen
-import com.example.infoquizapp.view.screen.SignUpScreen
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
---
+++ app/src/main/java/com/example/infoquizapp/view/component/mainscreencomponent/ProgressBarComp.kt
@@ -0,0 +1,38 @@
+package com.example.infoquizapp.view.component.mainscreencomponent
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProgressIndicatorDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun UserProgressBar(userLevel: Int, progressProvider: () -> Float) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Уровень $userLevel",
+            style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.Bold),
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        LinearProgressIndicator(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(8.dp),
+            color = MaterialTheme.colorScheme.primary,
+            progress = progressProvider
+        )
+    }
+}
