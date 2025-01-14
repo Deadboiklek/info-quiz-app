8edd393bcc23920ae20a29279c108aff23c67c7c
Deadboiklek <91292342+Deadboiklek@users.noreply.github.com>
2025-01-14 21:49:09 +0200

    Собрал ProdileScreen

--- app/src/main/java/com/example/infoquizapp/MainActivity.kt
+++ app/src/main/java/com/example/infoquizapp/MainActivity.kt
@@ -5,11 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.compose.AppTheme
-import com.example.infoquizapp.view.component.mainscreencomponent.ActivityCard
-import com.example.infoquizapp.view.component.mainscreencomponent.TabBar
-import com.example.infoquizapp.view.component.profilescreencomponent.TrophiesSection
-import com.example.infoquizapp.view.component.profilescreencomponent.UserProfileSection
-import com.example.infoquizapp.view.screen.HomeScreen
 
 class MainActivity : ComponentActivity() {
 
