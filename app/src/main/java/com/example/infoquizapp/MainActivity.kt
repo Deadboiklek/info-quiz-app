34ed02e9c827aeb0ffee4c2099dfaaadee30b8fb
Deadboiklek <91292342+Deadboiklek@users.noreply.github.com>
2025-01-17 18:16:43 +0200

    Сделал TheoryContentData.kt и собрал TheoryContentScreen.kt

--- app/src/main/java/com/example/infoquizapp/MainActivity.kt
+++ app/src/main/java/com/example/infoquizapp/MainActivity.kt
@@ -5,11 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.compose.AppTheme
-import com.example.infoquizapp.view.screen.LessonScreen
-import com.example.infoquizapp.view.screen.LoginScreen
-import com.example.infoquizapp.view.screen.MainScreen
-import com.example.infoquizapp.view.screen.ProfileScreen
-import com.example.infoquizapp.view.screen.TestScreen
+import com.example.infoquizapp.view.component.theorycontentscreen.data.TheoryContentData
 
 class MainActivity : ComponentActivity() {
 
@@ -18,7 +14,6 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppTheme() {
-
             }
         }
     }
---
+++ app/src/main/java/com/example/infoquizapp/view/component/theorycontentscreen/data/TheoryContentData.kt
@@ -0,0 +1,6 @@
+package com.example.infoquizapp.view.component.theorycontentscreen.data
+
+data class TheoryContentData(
+    val title: String,
+    val  body: String
+)
