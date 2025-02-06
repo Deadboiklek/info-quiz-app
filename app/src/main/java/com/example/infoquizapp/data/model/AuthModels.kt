5d8aa7f024bf8f88e8e5cd99de4e551a2a2e7948
Deadboiklek <91292342+Deadboiklek@users.noreply.github.com>
2025-02-06 20:52:00 +0200

    Поменял ApiAuthService.kt и AuthModels.kt, в соответствие с info-quiz-server

--- app/src/main/java/com/example/infoquizapp/data/model/AuthModels.kt
+++
@@ -1,7 +0,0 @@
-package com.example.infoquizapp.data.model
-
-data class RegisterRequest(val username: String, val email: String, val password: String)
-data class LoginRequest(val email: String, val password: String)
-data class AuthResponse(val accessToken: String, val refreshToken: String)
-data class ResetPasswordRequest(val email: String)
-data class ApiResponse(val message: String)
