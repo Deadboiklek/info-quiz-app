68408b0b40d3017ab6e5de1a4b6fd7fd04742b3b
Deadboiklek <91292342+Deadboiklek@users.noreply.github.com>
2025-02-12 19:19:57 +0200

    Изменил ApiProfileService.kt, добавил обработку ошибки

--- app/src/main/java/com/example/infoquizapp/data/profile/network/ApiProfileService.kt
+++ app/src/main/java/com/example/infoquizapp/data/profile/network/ApiProfileService.kt
@@ -7,11 +7,27 @@ import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 
+sealed class Response<T>{
+    data class Succes<T>(
+        val result: T
+    ): Response<T>()
+
+    data class Error<T>(
+        val error: ProfileError
+    ) : Response<T>()
+}
+
+sealed class ProfileError (val message: String?) {
+    data object GetProfileError: ProfileError("Ошибка получения профиля")
+}
+
 class ApiProfileService(private val client: HttpClient, private val baseUrl: String) {
 
-    suspend fun getProfile(token: String): UserOut {
-        return client.get("$baseUrl/profile") {
-            header("Authorization", "Bearer $token")
-        }.body()
+    suspend fun getProfile(token: String): Response<UserOut> {
+        return kotlin.runCatching {
+            Response.Succes(client.get("$baseUrl/profile") {
+                header("Authorization", "Bearer $token")
+            }.body<UserOut>())
+        }.getOrDefault(Response.Error(ProfileError.GetProfileError))
     }
 }
