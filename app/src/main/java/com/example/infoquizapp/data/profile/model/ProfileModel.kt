8e75a227ee1929f689be898e4275aa2f5361c1c6
Deadboiklek <91292342+Deadboiklek@users.noreply.github.com>
2025-02-12 17:47:21 +0200

    Добавил модель UserOut

---
+++ app/src/main/java/com/example/infoquizapp/data/profile/model/ProfileModel.kt
@@ -0,0 +1,10 @@
+package com.example.infoquizapp.data.profile.model
+
+data class UserOut(
+    val id: Int,
+    val username: String,
+    val email: String,
+    val level: Int,
+    val experience: Int,
+    val created_at: String
+)
