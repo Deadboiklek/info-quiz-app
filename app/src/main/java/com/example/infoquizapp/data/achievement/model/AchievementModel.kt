e54c2b9981182141796de23bf66a43aeac0f9bd0
Deadboiklek <91292342+Deadboiklek@users.noreply.github.com>
2025-02-13 17:52:18 +0200

    добавил AchievementModel.kt

---
+++ app/src/main/java/com/example/infoquizapp/data/achievement/model/AchievementModel.kt
@@ -0,0 +1,8 @@
+package com.example.infoquizapp.data.achievement.model
+
+data class AchievementOut(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val experienceBonus: Int
+)
