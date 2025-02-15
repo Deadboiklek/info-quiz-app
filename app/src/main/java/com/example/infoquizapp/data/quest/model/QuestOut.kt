365ff465b1696d15d49b3ec7a2eb2968d0eeb7b8
Deadboiklek <91292342+Deadboiklek@users.noreply.github.com>
2025-02-15 17:55:58 +0200

    добавил модель QuestOut.kt

---
+++ app/src/main/java/com/example/infoquizapp/data/quest/model/QuestOut.kt
@@ -0,0 +1,9 @@
+package com.example.infoquizapp.data.quest.model
+
+data class QuestOut(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val experienceReward: Int,
+    val isActive: Boolean
+)
