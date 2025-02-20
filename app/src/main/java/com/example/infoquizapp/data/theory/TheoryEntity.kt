4cc82c175aaae19bd2ed5b29526234ced813c3f2
Deadboiklek <91292342+Deadboiklek@users.noreply.github.com>
2025-02-20 17:47:40 +0200

    создал сущность TheoryEntity.kt

---
+++ app/src/main/java/com/example/infoquizapp/data/theory/TheoryEntity.kt
@@ -0,0 +1,11 @@
+package com.example.infoquizapp.data.theory
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "theory")
+data class TheoryEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val content: String
+)
