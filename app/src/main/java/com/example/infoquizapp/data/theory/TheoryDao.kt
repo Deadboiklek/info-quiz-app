74f211f52a43beeefe0ecc51f826dcc7219ef93c
Deadboiklek <91292342+Deadboiklek@users.noreply.github.com>
2025-02-20 18:00:18 +0200

    добавил интерфейс TheoryDao.kt

---
+++ app/src/main/java/com/example/infoquizapp/data/theory/TheoryDao.kt
@@ -0,0 +1,18 @@
+package com.example.infoquizapp.data.theory
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface TheoryDao {
+    @Query("SELECT * FROM theory WHERE id = :id LIMIT 1")
+    suspend fun getTheoryById(id: Int): TheoryEntity?
+
+    @Query("SELECT * FROM theory")
+    suspend fun getAllTheory(): List<TheoryEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTheory(theory: TheoryEntity)
+}
