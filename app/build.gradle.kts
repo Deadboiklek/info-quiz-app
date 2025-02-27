2ef02e9a296004dd5d8505ebeeaec4bf1a327c50
Deadboiklek <91292342+Deadboiklek@users.noreply.github.com>
2025-02-27 19:12:15 +0200

    добавил зависимость compose navigation, создал пока пустой AppNavGraph.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.androidx.navigation.compose)
 
     implementation (libs.androidx.material.icons.extended)
     implementation(libs.kodein.di)
