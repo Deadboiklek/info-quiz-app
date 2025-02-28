c6b504751b9ba55210ee67d11e43b6406d1b2c84
Deadboiklek <91292342+Deadboiklek@users.noreply.github.com>
2025-02-28 18:03:12 +0200

    Добавил UseCase для получения всех теорий

---
+++ app/src/main/java/com/example/infoquizapp/domain/theory/usecases/GetAllTheoryUseCase.kt
@@ -0,0 +1,10 @@
+package com.example.infoquizapp.domain.theory.usecases
+
+import com.example.infoquizapp.data.theory.TheoryEntity
+import com.example.infoquizapp.domain.theory.repository.TheoryRepository
+
+class GetAllTheoryUseCase( private val repository: TheoryRepository) {
+    suspend operator fun invoke(): List<TheoryEntity> {
+        return repository.getAllTheory()
+    }
+}
