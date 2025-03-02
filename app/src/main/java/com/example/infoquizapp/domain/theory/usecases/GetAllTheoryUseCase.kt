94c3e0f3bd3583a6555fce956266fb77a0b7aa66
Deadboiklek <91292342+Deadboiklek@users.noreply.github.com>
2025-03-02 17:40:08 +0200

    Добавил GetAllPracticeUseCase для получения всех практик

--- app/src/main/java/com/example/infoquizapp/domain/theory/usecases/GetAllTheoryUseCase.kt
+++ app/src/main/java/com/example/infoquizapp/domain/theory/usecases/GetAllTheoryUseCase.kt
@@ -3,7 +3,7 @@ package com.example.infoquizapp.domain.theory.usecases
 import com.example.infoquizapp.data.theory.TheoryEntity
 import com.example.infoquizapp.domain.theory.repository.TheoryRepository
 
-class GetAllTheoryUseCase( private val repository: TheoryRepository) {
+class GetAllTheoryUseCase(private val repository: TheoryRepository) {
     suspend operator fun invoke(): List<TheoryEntity> {
         return repository.getAllTheory()
     }
