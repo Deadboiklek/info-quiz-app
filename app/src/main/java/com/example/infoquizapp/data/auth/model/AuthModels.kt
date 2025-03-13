d7520f3680f4d202f5a99dc86195e8d32af9f437
Deadboiklek <91292342+Deadboiklek@users.noreply.github.com>
2025-03-13 15:54:18 +0200

    добавил обработку ошибок и логирование в ApiAuthService, обновил сериализацию TokenResponse

--- app/src/main/java/com/example/infoquizapp/data/auth/model/AuthModels.kt
+++ app/src/main/java/com/example/infoquizapp/data/auth/model/AuthModels.kt
@@ -1,6 +1,15 @@
 package com.example.infoquizapp.data.auth.model
 
-data class TokenResponse(val accessToken: String, val tokenType: String)
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+@Serializable
+data class TokenResponse(
+    @SerialName("access_token") val accessToken: String,
+    @SerialName("token_type") val tokenType: String
+)
 
+@Serializable
 data class UserCreate(val username: String, val email: String, val password: String)
+
+@Serializable
 data class UserLogin(val email: String, val password: String)
--- app/src/main/java/com/example/infoquizapp/di/diModule.kt
+++ app/src/main/java/com/example/infoquizapp/di/diModule.kt
@@ -40,6 +40,13 @@ import com.example.infoquizapp.presentation.theory.viewmodel.TheoryViewModel
 import com.example.infoquizapp.presentation.trial.viewmodel.TrialViewModel
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import org.kodein.di.DI
 import org.kodein.di.bind
 import org.kodein.di.instance
@@ -48,7 +55,20 @@ import org.kodein.di.singleton
 val appModule = DI.Module("appModule") {
 
     //ktor client
-    bind<HttpClient>() with singleton { HttpClient(CIO) }
+    bind<HttpClient>() with singleton {
+        HttpClient(CIO) {
+            install(Logging) {
+                logger = Logger.DEFAULT
+                level = LogLevel.ALL
+            }
+            install(ContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true
+                    isLenient = true
+                })
+            }
+        }
+    }
 
     //базовый юрл
     bind<String>("baseUrl") with singleton { "http://10.0.2.2:8000" }
