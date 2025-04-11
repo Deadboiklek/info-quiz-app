4fc599a4ee3ebaf632281ac8e75b08bfc0ccb806
Deadboiklek <91292342+Deadboiklek@users.noreply.github.com>
2025-04-11 17:50:21 +0200

    добавил функциональность для создания квиза: реализовал метод postQuiz в TeacherApiService, обновил интерфейс TeacherRepository и его реализацию TeacherRepositoryImpl

--- app/src/main/java/com/example/infoquizapp/data/teacher/network/TeacherApiService.kt
+++ app/src/main/java/com/example/infoquizapp/data/teacher/network/TeacherApiService.kt
@@ -1,11 +1,17 @@
 package com.example.infoquizapp.data.teacher.network
 
 import android.util.Log
+import com.example.infoquizapp.data.quiz.model.QuizOut
+import com.example.infoquizapp.data.teacher.model.TeacherCreateQuiz
 import com.example.infoquizapp.data.teacher.model.TeacherProfile
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.request.header
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 
 sealed class Response<T>{
     data class Succes<T>(
@@ -19,6 +25,7 @@ sealed class Response<T>{
 
 sealed class TeacherError (val message: String?) {
     data object GetTeacherProfileError: TeacherError("Ошибка получения профиля")
+    data object PostTeacherQuizError: TeacherError("Ошибка создания квиза")
 }
 
 class TeacherApiService(private val client: HttpClient, private val baseUrl: String) {
@@ -33,6 +40,22 @@ class TeacherApiService(private val client: HttpClient, private val baseUrl: Str
             onFailure = { ex ->
                 Log.e("TeacherApiService", "Ошибка запроса профиля: ${ex.localizedMessage}", ex)
                 Response.Error(TeacherError.GetTeacherProfileError)
+            }
+        )
+    }
+
+    suspend fun postQuiz(token: String, quiz: TeacherCreateQuiz): Response<QuizOut> {
+        return runCatching {
+            Response.Succes(client.post("$baseUrl/teacher/postquizzes") {
+                header("Authorization", "Bearer $token")
+                contentType(ContentType.Application.Json)
+                setBody(quiz)
+            }.body<QuizOut>())
+        }.fold(
+            onSuccess = { it },
+            onFailure = { ex ->
+                Log.e("TeacherApiService", "Ошибка создания квиза: ${ex.localizedMessage}", ex)
+                Response.Error(TeacherError.PostTeacherQuizError)
             }
         )
     }
--- app/src/main/java/com/example/infoquizapp/domain/teacher/repository/TeacherRepository.kt
+++ app/src/main/java/com/example/infoquizapp/domain/teacher/repository/TeacherRepository.kt
@@ -1,8 +1,11 @@
 package com.example.infoquizapp.domain.teacher.repository
 
+import com.example.infoquizapp.data.quiz.model.QuizOut
+import com.example.infoquizapp.data.teacher.model.TeacherCreateQuiz
 import com.example.infoquizapp.data.teacher.model.TeacherProfile
 import com.example.infoquizapp.data.teacher.network.Response
 
 interface TeacherRepository {
     suspend fun getTeacherProfile(token: String): Response<TeacherProfile>
+    suspend fun postTeacherQuiz(token: String, quiz: TeacherCreateQuiz) : Response<QuizOut>
 }
