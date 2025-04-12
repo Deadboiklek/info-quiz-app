fd9bdbd2b175f3630779f9e75f63b4a7cdeb82a5
Deadboiklek <91292342+Deadboiklek@users.noreply.github.com>
2025-04-12 18:48:39 +0200

    добавил методы для получения и удаления квизов: реализовал getTeacherQuizzes и deleteTeacherQuiz в TeacherRepository и TeacherRepositoryImpl

--- app/src/main/java/com/example/infoquizapp/data/teacher/repository/TeacherRepositoryImpl.kt
+++ app/src/main/java/com/example/infoquizapp/data/teacher/repository/TeacherRepositoryImpl.kt
@@ -7,4 +7,6 @@ import com.example.infoquizapp.domain.teacher.repository.TeacherRepository
 class TeacherRepositoryImpl(private val teacherApiService: TeacherApiService) : TeacherRepository {
     override suspend fun getTeacherProfile(token: String) = teacherApiService.getTeacherProfile(token)
     override suspend fun postTeacherQuiz(token: String, quiz: TeacherCreateQuiz) = teacherApiService.postQuiz(token, quiz)
+    override suspend fun getTeacherQuizzes(token: String) = teacherApiService.getTeacherQuizzes(token)
+    override suspend fun deleteTeacherQuiz(token: String, quizId: Int) = teacherApiService.deleteTeacherQuiz(token, quizId)
 }
--- app/src/main/java/com/example/infoquizapp/domain/teacher/repository/TeacherRepository.kt
+++ app/src/main/java/com/example/infoquizapp/domain/teacher/repository/TeacherRepository.kt
@@ -8,4 +8,6 @@ import com.example.infoquizapp.data.teacher.network.Response
 interface TeacherRepository {
     suspend fun getTeacherProfile(token: String): Response<TeacherProfile>
     suspend fun postTeacherQuiz(token: String, quiz: TeacherCreateQuiz) : Response<QuizOut>
+    suspend fun getTeacherQuizzes(token: String): Response<List<QuizOut>>
+    suspend fun deleteTeacherQuiz(token: String, quizId: Int): Response<QuizOut>
 }
