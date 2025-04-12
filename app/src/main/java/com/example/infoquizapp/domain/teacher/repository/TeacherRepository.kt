53c17a8cb0b8f069b4772b51e65660aca9a9674b
Deadboiklek <91292342+Deadboiklek@users.noreply.github.com>
2025-04-12 21:37:24 +0200

    добавил методы для получения информации о студентах и статистики: реализовал getTeacherStudents и getStudentStatistics в TeacherRepository и TeacherRepositoryImpl

--- app/src/main/java/com/example/infoquizapp/domain/teacher/repository/TeacherRepository.kt
+++ app/src/main/java/com/example/infoquizapp/domain/teacher/repository/TeacherRepository.kt
@@ -1,6 +1,8 @@
 package com.example.infoquizapp.domain.teacher.repository
 
 import com.example.infoquizapp.data.quiz.model.QuizOut
+import com.example.infoquizapp.data.teacher.model.StudentInfo
+import com.example.infoquizapp.data.teacher.model.StudentStatistics
 import com.example.infoquizapp.data.teacher.model.TeacherCreateQuiz
 import com.example.infoquizapp.data.teacher.model.TeacherProfile
 import com.example.infoquizapp.data.teacher.network.Response
@@ -10,4 +12,6 @@ interface TeacherRepository {
     suspend fun postTeacherQuiz(token: String, quiz: TeacherCreateQuiz) : Response<QuizOut>
     suspend fun getTeacherQuizzes(token: String): Response<List<QuizOut>>
     suspend fun deleteTeacherQuiz(token: String, quizId: Int): Response<QuizOut>
+    suspend fun getTeacherStudents(token: String): Response<List<StudentInfo>>
+    suspend fun getStudentStatistics(token: String, studentId: Int): Response<StudentStatistics>
 }
